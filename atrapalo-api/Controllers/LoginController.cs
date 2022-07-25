9b5aa269698ded60f14df652d8146ba238034297
claudio vargas <claudiovargaslillo@gmail.com>
2022-07-24 20:44:17 -0400

    inicial

---
+++ atrapalo-api/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using atrapalo_api.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace atrapalo_api.Controllers
+{
+    [ApiController]
+    [Route("api/login")]
+    public class LoginController : Controller
+    {
+
+
+        [HttpGet]
+        public Response Get() {
+            
+            var persona = new Persona()
+            {
+                Rut = 16268294,
+                Dv = "6",
+                Nombres = "Claudio Ivan",
+                Apellidos = "Vargas Lillo",
+                Correo = "claudiovargaslillo@gmail.com"
+
+            };
+            var response = new Response()
+            {
+                Status = 200,
+                Data = persona,
+            };
+            return response; 
+        
+        }
+       
+    }
+}
---
+++ atrapalo-api/Entities/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace atrapalo_api.Entities
+{
+    public class Response
+    {
+        public int Status { get; set; }
+        public object Data { get; set; }
+        public string Messagge { get; set; }
+    }
+}
