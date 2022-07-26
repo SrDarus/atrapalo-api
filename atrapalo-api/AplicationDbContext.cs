ae56ff886705f7aa63098dcb6e494620872c0a33
claudio vargas <claudio.vargasli@tecnova.cl>
2022-07-25 23:39:00 -0400

    se genera dbset y string de coneccion

---
+++ atrapalo-api/AplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace atrapalo_api
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- atrapalo-api/Program.cs
+++ atrapalo-api/Program.cs
@@ -1,11 +1,18 @@
+using atrapalo_api;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+var connectionString = builder.Configuration.GetConnectionString("defaultConnection");
+builder.Services.AddDbContext<AplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
