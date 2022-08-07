725d602410b6f005d4b753e9f419e6d295c362f4
claudio vargas <claudio.vargasli@tecnova.cl>
2022-08-07 02:32:02 -0400

    startup.cs

--- atrapalo-api/Program.cs
+++ atrapalo-api/Program.cs
@@ -4,32 +4,14 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var startup = new Startup(builder.Configuration);
 
-builder.Services.AddControllers().AddJsonOptions( x => 
-    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+startup.ConfigureServices(builder.Services);
 
-var connectionString = builder.Configuration.GetConnectionString("defaultConnection");
-builder.Services.AddDbContext<AplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+//var connectionString = builder.Configuration.GetConnectionString("defaultConnection");
+//builder.Services.AddDbContext<AplicationDbContext>(options =>
+//    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+startup.Configure(app, app.Environment);
 app.Run();
---
+++ atrapalo-api/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+
+namespace atrapalo_api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+            services.AddDbContext<AplicationDbContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+            
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddSwaggerGen( c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApíAtrapalo", Version = "v0"});
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endPoints =>
+            {
+                endPoints.MapControllers();
+            });
+        }
+    }
+}
